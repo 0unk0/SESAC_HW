22450ff69b35afc7de17351dadd3cd6a39bc89fe
yunk._.k <hamndo@naver.com>
2023-11-01 17:53:34 +0900

    sort 인덱스 수정

--- HW7/HW7_sort.js
+++ HW7/HW7_sort.js
@@ -3,7 +3,7 @@ list = [4, 2, 7, 1, 9, 5];
 function sort(list){
     for (let i = 0; i < list.length; i++){
         min = list[i];
-        for(let j = i; j < list.length; j++){
+        for(let j = i+1; j < list.length; j++){
             if(list[j] < min){
                 min = list[j];
                 list[j] = list[i];
