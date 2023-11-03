b27b28d4b30f491989638eac75e9018a9570e925
yunk._.k <hamndo@naver.com>
2023-11-03 23:37:41 +0900

    HW8_예외처리 수정

--- HW8_calculator_cli/HW8_calculator_cli.js
+++ HW8_calculator_cli/HW8_calculator_cli.js
@@ -10,42 +10,54 @@ const rl = createInterface({
     output: process.stdout, 
 });
 
-rl.question("Select Calculator Mode:\n1. Engineering Calculator\n2. Standard Calculaot\n3. Programmer Calculator\nEnter the mode (1/2/3) : ", (input) => { 
+console.log("Select Calculator Mode:\n1. Engineering Calculator\n2. Standard Calculaot\n3. Programmer Calculator\n");
+
+
+rl.question("Enter the mode (1/2/3) : ", (input) => { 
     const mode = parseInt(input);
-    if(mode > 3 || mode < 1 || typeof mode !== "number"){ 
-        console.log("입력 오류");
-        rl.close();
-    } else{
+    if(mode <=3 && mode >= 1){ 
         rl.question("Enter first number: ", (input) => { // 첫번째 숫자 입력 받기
             const num1 = parseInt(input);
-            rl.question("Enter operator (+, -, *, /): ", (input) => { // 연산자 입력 받기
-                const operator = input;
-                if(operator !== '+' && operator !== '-' && operator !== '*' && operator && '/' ){
-                    console.log("입력 오류")
-                    rl.close();  
-                } else{
-                    rl.question("Enter second number: ", (input) => { // 두번째 숫자 입력 받기
-                        const num2 = parseInt(input);
-                        
-                        switch(mode){
-                            case 1:
-                                const genericCalculator = new genericCal();
-                                cal(genericCalculator,operator, num1, num2);
-                                break;
-                            case 2:
-                                const engineeringCalculator = new engineeringCal(); 
-                                cal(engineeringCalculator, operator, num1, num2);
-                                break;
-                            case 3:
-                                const programmingCalculator = new programmingCal();
-                                cal(programmingCalculator, operator, num1, num2);
-                                break;
-                        }
+            if(!isNaN(num1)){
+                rl.question("Enter operator (+, -, *, /): ", (input) => { // 연산자 입력 받기
+                    const operator = input;
+                    if(operator === '+' || operator === '-' || operator === '*' || operator === '/' ){
+                        rl.question("Enter second number: ", (input) => { // 두번째 숫자 입력 받기
+                            const num2 = parseInt(input);
+                            if(!isNaN(num2)){
+                                switch(mode){
+                                    case 1:
+                                        const genericCalculator = new genericCal();
+                                        cal(genericCalculator,operator, num1, num2);
+                                        break;
+                                    case 2:
+                                        const engineeringCalculator = new engineeringCal(); 
+                                        cal(engineeringCalculator, operator, num1, num2);
+                                        break;
+                                    case 3:
+                                        const programmingCalculator = new programmingCal();
+                                        cal(programmingCalculator, operator, num1, num2);
+                                        break;
+                                }
+                                rl.close();
+                            }else{
+                                console.log("숫자 입력 오류");
+                                rl.close();
+                            }
+                        });
+                    } else{
+                        console.log("연산자 입력 오류");
                         rl.close();
-                    });
-                }
-            });
+                    }
+                });
+            } else{
+                console.log("숫자 입력 오류");
+                rl.close();
+            }
         });
+        } else{
+        console.log("입력 오류");
+        rl.close();  
     }   
 });
 
