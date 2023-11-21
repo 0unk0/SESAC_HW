ae7da73521525eaa430ab16dfcf0cc379cfa9052
yunk._.k <hamndo@naver.com>
2023-11-22 00:43:09 +0900

    [FEAT] 1, 2, 3, 4, 5 - 1. 페이징 - 2. 페이지 이동 - 3. 앞뒤 버튼 - 4. 상세 페이지 - 5. 검색

--- HW14_userTable/app.js
+++ HW14_userTable/app.js
@@ -17,44 +17,26 @@ nunjucks.configure("views", {
 app.set("view engine", "html");
 app.use("/", express.static("static"));
 
-// 시간 측정
-app.use((req, res, next) => {
-  const start = Date.now();
-
-  // 나중에 동작할 리스너 등록
-  res.on("finish", () => {
-    const end = Date.now();
-    const duration = end - start;
-
-    console.log(`요청 ${req.path} 에서 응답까지는 ${duration}ms 이 소요되었습니다.`)
-  })
-  next();
-})
-
 async function loadDataIntoMemory() {
   return new Promise((resolve, reject) => {
     fs.createReadStream("user.csv")
-    .pipe(csv({ skipLines: 0} ))
-    .on("headers", (header) => {
+      .pipe(csv({ skipLines: 0 }))
+      .on("headers", (header) => {
         headers.push(...header);
-    })
-    .on("data", (data) => {
-      {csvData.push(data)}    
-      // 공백 제거
-      // const cleanRow = {};
-      // for (const [key, value] of Object.entries(data)) {
-      //   cleanRow[key.trim()] = value.trim();
-      // }
-      // csvData.push(cleanRow);
-    })
-    .on("end", () => {
-      console.log("파일 다 읽었다람쥐");
-      resolve();
-    })
-    .on("error", (error) => {
-      console.log("파일 읽기 오류", error);
-      reject(error);
-    });
+      })
+      .on("data", (data) => {
+        {
+          csvData.push(data);
+        }
+      })
+      .on("end", () => {
+        console.log("파일 다 읽었다람쥐");
+        resolve();
+      })
+      .on("error", (error) => {
+        console.log("파일 읽기 오류", error);
+        reject(error);
+      });
   });
 }
 
@@ -63,18 +45,43 @@ async function startserver() {
   await loadDataIntoMemory();
 
   app.get("/", (req, res) => {
-    const itemsPerPage = 10;
-
-    const page = req.query.page || 1; 
+    const itemsPerPage = 100;
+    const page = req.query.page || 1;
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
 
+    const Data = csvData.slice(startIndex, endIndex);
     const totalPages = Math.ceil(csvData.length / itemsPerPage);
 
-    const Data = csvData.slice(startIndex, endIndex);
-    res.render("index", { totalPages: totalPages, page: parseInt(page), headers: headers, Data: Data });
+    res.render("index", {
+      totalPages: totalPages,
+      page: parseInt(page),
+      headers: headers,
+      Data: Data,
+    });
   });
-  
+
+  app.get("/user", (req, res) => {
+    const itemsPerPage = 100;
+    const page = req.query.page || 1;
+
+    const findData = csvData.filter((csvData) => csvData.Name.includes(req.query.name));
+    const totalPages = Math.ceil(findData.length / itemsPerPage);
+
+    res.render("index", {
+      totalPages: totalPages,
+      page: parseInt(page),
+      headers: headers,
+      Data: findData,
+    });
+  });
+
+  app.get("/user/:id", (req, res) => {
+    const Data = [csvData.find((csvData) => csvData.Id === req.params.id)];
+
+    res.render("userDetail", { headers: headers, Data: Data });
+  });
+
   app.listen(port, () => {
     console.log(`서버가 ${port} 에 생성되었습니다`);
   });
