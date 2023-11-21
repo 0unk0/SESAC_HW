bad34bd66f62e223f230ba6a5f891a35a34112f4
yunk._.k <hamndo@naver.com>
2023-11-21 19:34:21 +0900

    [11/21] 리뷰 + 페이징 처리

--- HW14_userTable/app.js
+++ HW14_userTable/app.js
@@ -6,6 +6,9 @@ const csv = require("csv-parser");
 const app = express();
 const port = 3000;
 
+const csvData = [];
+const headers = [];
+
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
@@ -14,20 +17,67 @@ nunjucks.configure("views", {
 app.set("view engine", "html");
 app.use("/", express.static("static"));
 
-app.get("/", (req, res) => {
-  const csvData = [];
-  const headers = ["Id", "Name", "Gender", "Age", "Birthdate", "Address"];
+// 시간 측정
+app.use((req, res, next) => {
+  const start = Date.now();
 
-  fs.createReadStream("user.csv")
-    .pipe(csv({ skipLines: 0}, headers ))
+  // 나중에 동작할 리스너 등록
+  res.on("finish", () => {
+    const end = Date.now();
+    const duration = end - start;
+
+    console.log(`요청 ${req.path} 에서 응답까지는 ${duration}ms 이 소요되었습니다.`)
+  })
+  next();
+})
+
+async function loadDataIntoMemory() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream("user.csv")
+    .pipe(csv({ skipLines: 0} ))
+    .on("headers", (header) => {
+        headers.push(...header);
+    })
     .on("data", (data) => {
-        if (csvData.length < 20) {csvData.push(data)}
+      {csvData.push(data)}    
+      // 공백 제거
+      // const cleanRow = {};
+      // for (const [key, value] of Object.entries(data)) {
+      //   cleanRow[key.trim()] = value.trim();
+      // }
+      // csvData.push(cleanRow);
     })
     .on("end", () => {
-      res.render("index", { headers: headers, csvData: csvData });
+      console.log("파일 다 읽었다람쥐");
+      resolve();
+    })
+    .on("error", (error) => {
+      console.log("파일 읽기 오류", error);
+      reject(error);
     });
-});
+  });
+}
 
-app.listen(port, () => {
-  console.log(`서버가 ${port} 에 생성되었습니다`);
-});
+// 서버 시작
+async function startserver() {
+  await loadDataIntoMemory();
+
+  app.get("/", (req, res) => {
+    const itemsPerPage = 10;
+
+    const page = req.query.page || 1; 
+    const startIndex = (page - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+
+    const totalPages = Math.ceil(csvData.length / itemsPerPage);
+
+    const Data = csvData.slice(startIndex, endIndex);
+    res.render("index", { totalPages: totalPages, page: parseInt(page), headers: headers, Data: Data });
+  });
+  
+  app.listen(port, () => {
+    console.log(`서버가 ${port} 에 생성되었습니다`);
+  });
+}
+
+startserver();
