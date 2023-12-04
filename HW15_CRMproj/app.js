66b60c3bdfeb63eb63fe7744fd2b22e41fba3a62
yunk._.k <hamndo@naver.com>
2023-12-04 21:37:44 +0900

    [STYLE] 주석 제거

--- HW15_CRMproj/app.js
+++ HW15_CRMproj/app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
-// const sqlite = require("sqlite3");
-// const db = new sqlite.Database("CRMproj.db");
 
 const app = express();
 const PORT = 3000;
@@ -11,39 +9,11 @@ nunjucks.configure("views", {
   express: app,
 });
 
-const mainRoutes = require("./src/routes/mainRoutes.js");
-app.use("/", mainRoutes);
-
 app.use("/", express.static("public"));
 app.set("view engine", "html");
 
-// app.get("/", (req, res) => {
-//   res.render("user");
-// });
-
-// app.get("/user", (req, res) => {
-//   // const query = db.prepare("SELECT * FROM users");
-//   const headers = ["Id", "Name", "Gender", "Age", "Birthdate"];
-//   db.all("SELECT * FROM users", (err, rows) => {
-//     res.render("user", { headers: headers, userTable: rows });
-//   });
-// });
-
-// app.get("/order", (req, res) => {
-//   res.render("order");
-// });
-
-// app.get("/orderitem", (req, res) => {
-//   res.render("orderitem");
-// });
-
-// app.get("/item", (req, res) => {
-//   res.render("item");
-// });
-
-// app.get("/store", (req, res) => {
-//   res.render("store");
-// });
+const mainRoutes = require("./src/routes/mainRoutes.js");
+app.use("/", mainRoutes);
 
 app.listen(PORT, (req, res) => {
   console.log(`서버가 ${PORT}번 포트 대기중...`);
