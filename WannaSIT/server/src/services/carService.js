95331c1b782eee0e0997c8a49aefd427e3ff3428
yunk._.k <hamndo@naver.com>
2024-01-04 22:55:55 +0900

    [FEAT] 호차 정보-칸 유지 - 칸 유지 시 착석 가능성 분류 : calculateChanceByCount() - 응답 데이터로 가공(임시)    - station, tip>feature/character, chance값 존재
    * traffic, tip>character특징, highCars 값 : 상민오빠한테 넘겨받기

--- WannaSIT/server/src/services/carService.js
+++ WannaSIT/server/src/services/carService.js
@@ -5,10 +5,21 @@ function calculateRanking(routeDetail) {
 
   // 우선순위 : minCount  > index(역)
   sortArray(minCountByCar);
-  console.log(minCountByCar);
 
-  const carRank = transformForResponse(minCountByCar);
+  const carRank = transformForCarResponse(minCountByCar);
   return carRank;
+}
+
+// 착석 가능성 분류 함수 // 상수
+function calculateChanceByCount(routeDetail, carNumber) {
+  const countByCar = gatherCountByCar(routeDetail);
+  const countCar = countByCar[carNumber - 1];
+
+  countCar.map((car, index) => {
+    countCar[index] = car < 54 ? "높음" : car <= 64 ? "중간" : "낮음";
+  });
+
+  return countCar;
 }
 
 // 경로까지 칸 별 예측 인원 모으는 함수
@@ -32,6 +43,7 @@ function findMinCountByCar(countByCar) {
 }
 
 function sortArray(array) {
+  console.log(array);
   array.sort((a, b) => {
     // 출발역에서 앉을 수 있다면 0순위
     if (a.stationIndex === 0 && a.minCount <= 34) {
@@ -43,9 +55,10 @@ function sortArray(array) {
       return a.minCount - b.minCount;
     }
   });
+  console.log(array);
 }
 
-function transformForResponse(minCountByCar) {
+function transformForCarResponse(minCountByCar) {
   return minCountByCar.map(({ carNum, stationIndex, minCount }) => {
     const isSeatAvailable = minCount <= 34 ? 1 : 0;
     return {
@@ -55,62 +68,5 @@ function transformForResponse(minCountByCar) {
     };
   });
 }
-// // 랭킹 계산 함수
-// function calculateRanking(routeDetail) {
-//   const countByCar = gatherCountByCar(routeDetail);
-//   const { canSit, cannotSit } = findMinCountByCar(countByCar);
 
-//   // 앉을 수 있는 경우 우선순위 : index(역) > minCount | 앉을 수 없는 경우 우선순위 : minCount  > index(역)
-//   sortArray(canSit, "index", "minCount");
-//   sortArray(cannotSit, "minCount", "index");
-
-//   const carRank = [...canSit, ...cannotSit];
-//   console.log(carRank);
-
-//   return carRank.map(({ carNum, stationIndex }) => ({ carNum, stationIndex }));
-// }
-
-// // 경로까지 칸 별 예측 인원 모으는 함수
-// function gatherCountByCar(routeData) {
-//   return Array.from({ length: routeData[0].length }, (_, index) => routeData.map((station) => station[index].estimated_count));
-// }
-
-// // 분류 방법: 앉을 수 O -> 앉을 수 X
-// function findMinCountByCar(countByCar) {
-//   const STANDARD_VALUE = 34;
-
-//   const canSit = [];
-//   const cannotSit = [];
-
-//   countByCar.forEach((car, index) => {
-//     let carNum = index + 1;
-
-//     let stationIndex = car.findIndex((count) => count <= STANDARD_VALUE);
-//     let minCount;
-
-//     // 앉을 수 있음
-//     if (stationIndex >= 0) {
-//       minCount = car[stationIndex];
-//       canSit.push({ carNum, stationIndex, minCount });
-//     } else {
-//       // 앉을 수 없음 => 최소값
-//       minCount = Math.min(...car);
-//       stationIndex = car.indexOf(minCount);
-//       cannotSit.push({ carNum, stationIndex, minCount });
-//     }
-//   });
-
-//   return { canSit, cannotSit };
-// }
-
-// // 우선순위에 따라 정렬
-// function sortArray(array, primary, secondary) {
-//   array.sort((a, b) => {
-//     if (a[primary] === b[primary]) {
-//       return a[secondary] - b[secondary];
-//     }
-//     return a[primary] - b[primary];
-//   });
-// }
-
-export { calculateRanking };
+export { calculateRanking, calculateChanceByCount };
