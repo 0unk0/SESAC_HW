f6a0152ceaf91089df0dc4af865bdbc4d2c1988a
yunk._.k <hamndo@naver.com>
2024-01-28 19:40:31 +0900

    [FEAT] 게시글 API(pull) + 댓글 API

---
+++ WannaSIT/server/src/utils/getAuthorId.js
@@ -0,0 +1,22 @@
+import { getConnection, executeQuery, endConnection } from "../services/databaseService.js";
+
+async function getAuthorId(table, id) {
+  const connection = await getConnection();
+  const params = [table, id];
+  const query = `
+  SELECT user_id
+  FROM ?
+  WHERE id = ?`;
+
+  try {
+    const [rows, fields] = await executeQuery(connection, query, params);
+    return rows[0].user_id;
+  } catch (err) {
+    console.error("작성자 ID 조회 실패", err);
+    return false;
+  } finally {
+    endConnection(connection);
+  }
+}
+
+export default getAuthorId;
